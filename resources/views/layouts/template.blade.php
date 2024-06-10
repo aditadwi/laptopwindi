0e3ae748a21347b7f14b2951f2ea02cfd3c768e5
selpina10 <deayod8@gmail.com>
2024-06-10 12:23:53 +0700

    Mengubah nama

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -43,7 +43,7 @@
         <span class="dropdown-item dropdown-header">User Menu</span> 
              <div class="dropdown-divider"></div> 
               <a href="#" class="dropdown-item"> 
-                 <i class="fas fa-user mr-2">syafira windi</i>
+                 <i class="fas fa-user mr-2">windi syafira</i>
              <span class="float-right text-muted text-sm"></span> 
              </a> 
                         
